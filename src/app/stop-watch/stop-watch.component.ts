94e60ef04c25b6da8412fbcb43f3bdbe51402735
DmytroFilonenko <filonenkodmytrodm@gmail.com>
2025-01-02 13:36:41 +0000

    migrate logic to service

---
+++ src/app/stop-watch/stop-watch.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe, DatePipe } from '@angular/common';
+
+import { TimerService } from './timer.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-stop-watch',
+  templateUrl: './stop-watch.component.html',
+  imports: [AsyncPipe, DatePipe]
+})
+export class StopWatch implements OnInit {
+  private timerService: TimerService = inject(TimerService);
+
+  public ngOnInit(): void {
+    this.timerService.handleWaitClick(() => this.timerService.togglePause());
+  }
+
+  public get userTimer(): Observable<Date> {
+    return this.timerService.getUserTimer$;
+  }
+
+  public get isTimerPaused(): boolean {
+    return this.timerService.getIsPaused;
+  }
+
+  public get isTimerStarted(): boolean {
+    return this.timerService.getIsTimerStarted;
+  }
+
+  public onToggleTimerMode(): void {
+      this.timerService.togglePause();
+  }
+
+  public onReset(): void {
+    this.timerService.resetTimer();
+  }
+
+  public onWaitClick(): void {
+    this.timerService.emitWaitClick();
+  }
+}
